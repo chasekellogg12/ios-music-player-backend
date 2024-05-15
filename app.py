3c0d610dc2c085a695400f18ad4d7280ab8c03f4
chasekellogg12 <chasekellogg12@gmail.com>
2024-05-15 12:21:18 -0700

    removed main

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def get_audio_link():
         print(f"Error: {str(e)}")
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
